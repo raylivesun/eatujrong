e9d4398b9a192be3af78b7179db809d0d69a8d14
raylivesun <162396999+raylivesun@users.noreply.github.com>
2025-05-16 12:24:45 -0300

    the main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <array>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#include <c++/11/array>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!";
+    return 0;    
+}
+
+
